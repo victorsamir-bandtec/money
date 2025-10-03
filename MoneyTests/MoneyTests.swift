7d660fafb781134b2b4b88dbb6c984b696fe0843
victorsamir-bandtec <victor.bsilva@bandtec.com.br>
2025-10-03 14:31:41 -0300

    Initial Commit

---
+++ MoneyTests/MoneyTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoneyTests.swift
+//  MoneyTests
+//
+//  Created by victor samir on 03/10/25.
+//
+
+import Testing
+
+struct MoneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
